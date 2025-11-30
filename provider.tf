743fd32a22d77fd803257b1aa74617c8662b6caa
Nagashankar <nagashankar0848@gmail.com>
2025-11-30 23:07:37 +0530

    jenkins

--- provider.tf
+++ provider.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
-    key    = "roboshop-dev-cicd"
-    region = "us-east-1"
-    encrypt = true
-  }
+  # backend "s3" {
+  #   bucket  = "latest-84s-remote-state-dev"
+  #   key     = "roboshop-dev-cicd"
+  #   region  = "us-east-1"
+  #   encrypt = true
+  # }
 }
 
 provider "aws" {
